15301ac950995f602ad38651a74678453a3a9cc2
Adrian Warman <warmana@gmail.com>
2017-12-12 10:40:30 +0000

    Don't check if there is nothing to check.

--- scripts/checkSpelling.sh
+++ scripts/checkSpelling.sh
@@ -5,6 +5,13 @@ MARKDOWN_FILES_CHANGED=`(git diff --name-only $TRAVIS_COMMIT_RANGE || true) | gr
 echo "Running spelling check for files:"
 echo $MARKDOWN_FILES_CHANGED
 
+if [ -z "$MARKDOWN_FILES_CHANGED" ]
+then
+    echo -e "No markdown file to check."
+
+    exit 0;
+fi
+
 # cat all markdown files that changed
 TEXT_CONTENT_WITHOUT_METADATA=`cat $(echo "$MARKDOWN_FILES_CHANGED" | sed -E ':a;N;$!ba;s/\n/ /g')`
 # remove metadata tags
@@ -24,11 +31,11 @@ NB_MISSPELLED=`echo "$MISSPELLED" | wc -l`
 
 if [ "$NB_MISSPELLED" -gt 0 ]
 then
-    echo -e "$RED>> Words that might be misspelled, please check:$NC"
+    echo -e "Words that might be misspelled, please check:"
     MISSPELLED=`echo "$MISSPELLED" | sed -E ':a;N;$!ba;s/\n/, /g'`
     echo "$MISSPELLED"
     COMMENT="$NB_MISSPELLED words might be misspelled, please check them: $MISSPELLED"
 else
     COMMENT="No spelling errors, congratulations!"
-    echo -e "$GREEN>> $COMMENT $NC"
+    echo -e "$COMMENT"
 fi
